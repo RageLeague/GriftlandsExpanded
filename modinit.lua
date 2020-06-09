7e4bb0906344c0d94ca39103fc4bf4397b1a14f1
JuniorElder <64610396+JuniorElder@users.noreply.github.com>
2020-06-09 07:56:29 +0200

    Add files via upload

--- modinit.lua
+++ modinit.lua
@@ -4,13 +4,20 @@ local function OnLoad()
     local self_dir = "JuniorElderExpandedMod:mod_content/"
     local LOAD_FILE_ORDER =
     {
+        -- COMPLEMENTARY CARDS AND DEFINITIONS
         "basic_negotiation_mod",
         "basic_actions_mod",
         "battle_conditions_mod",
+        "items_mod",
+        -- CHARACTER CARDS
         "sal_negotiation_mod",
         "sal_battle_mod",
         --"sal_battle_grafts_mod",
-        "custom_boss_mod",
+        -- CUSTOM BOSSES
+        "sal_boss_1_mod",
+        "sal_boss_2_mod",
+        "sal_boss_3_mod",
+        -- QUESTS, CONVOS
         "convo_override",
     }
     for k, filepath in ipairs(LOAD_FILE_ORDER) do
